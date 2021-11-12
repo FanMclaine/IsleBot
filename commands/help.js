90996ca241f4bceb80fcc696ff4b545f7d896dac
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2021-11-12 08:49:44 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ data.setDescription("Shows available commands!");
 const execute = async function(interaction)
 {
   await interaction.reply({
-        content: `All available commands:\n/help - Shows available commands!\n/roll - Rolls a random number between 0-100, both inclusive.\n/timecheck - Shows the current time in isleward.\nThis bot is not complete, feel free to contribute by creating a pull request on https://github.com/Carnagion/IsleBot :D`,
+        content: `All available commands:\n/help - Shows available commands!\n/roll - Rolls a random number between 0-100, both inclusive.\n/timecheck - Shows the current time in isleward.\nThis bot is not yet complete, feel free to contribute by creating a pull request on https://github.com/Carnagion/IsleBot :D`,
   });
 }  
 
